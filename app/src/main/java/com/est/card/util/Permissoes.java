b9ce94386e9cebcb0d14ecb317bef5a5dfb7d09a
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2018-05-04 07:36:02 -0300

    Classe que gerencia as permissões

---
+++ app/src/main/java/com/est/card/util/Permissoes.java
@@ -0,0 +1,32 @@
+package com.est.card.util;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permissoes {
+
+    public static boolean validate(Activity activity, int requestCode, String... permissions) {
+        List<String> list = new ArrayList<String>();
+        for (String permission : permissions) {
+
+            boolean ok = ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED;
+            if (! ok ) {
+                list.add(permission);
+            }
+        }
+        if (list.isEmpty()) {
+            return true;
+        }
+
+        String[] newPermissions = new String[list.size()];
+        list.toArray(newPermissions);
+        ActivityCompat.requestPermissions(activity, newPermissions, 1);
+
+        return false;
+    }
+}
